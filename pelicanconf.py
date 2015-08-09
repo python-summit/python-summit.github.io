8f0b9b1e783b7e0af2c3cb5bb05c43e07accedd4
Cesar Desales <cesar.desales@webrepublic.ch>
2015-08-09 21:13:59 +0200

    Boilerplate

---
+++ pelicanconf.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'@ccdesales'
+SITENAME = u'Swiss Python Conference'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+	#('Pelican', 'http://getpelican.com/'),
+)
+
+# Social widget
+SOCIAL = (
+	#('You can add links in your config file', '#'),
+        #('Another social link', '#'),
+)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+#THEME = "/Users/cdesales/git/pelican-themes/sundown"
+#THEME = "/Users/cdesales/git/pelican-themes/storm"
+#THEME = "/Users/cdesales/git/pelican-themes/blue-penguin"
+THEME = "/Users/cdesales/git/pelican-themes/relapse"
+
+OUTPUT_PATH="/Users/cdesales/git/swisspycon"
+
