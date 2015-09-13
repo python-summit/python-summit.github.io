fe961ff9f1d6ca659beff6bf02abcd032e78b889
Danilo Bargen <mail@dbrgn.ch>
2015-09-13 22:19:36 +0200

    Fixed footer

--- pelicanconf.py
+++ pelicanconf.py
@@ -64,7 +64,7 @@ NEST_SITEMAP_ATOM_LINK = u'Atom Feed'
 NEST_SITEMAP_RSS_LINK = u'RSS Feed'
 NEST_SOCIAL_COLUMN_TITLE = u'Social'
 NEST_LINKS_COLUMN_TITLE = u'Links'
-NEST_COPYRIGHT = u'&copy; blogname 2015'
+NEST_COPYRIGHT = u'&copy; 2015 Swiss Python Summit'
 
 # Footer optional
 NEST_FOOTER_HTML = ''
